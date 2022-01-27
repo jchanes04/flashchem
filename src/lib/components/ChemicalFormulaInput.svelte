c8042bc992d953e9a53af845349b887e81ef76a3
WaterNinja101 <coolwolfs8@gmail.com>
2022-01-27 06:41:00 -0500

    more chemical formula input work

--- src/lib/components/ChemicalFormulaInput.svelte
+++ src/lib/components/ChemicalFormulaInput.svelte
@@ -19,7 +19,6 @@
         type: "normal"
     }]
     let contentLength = 0
-    let cursorPosition = 0
 
     let selection = [0, 0]
     
@@ -35,74 +34,214 @@
         const cursorCoordinates = getCursorCoordinates()
         const currentSection = sections[cursorCoordinates[0]]
         if (currentSection.type === "normal") {
-            if (e.data === "+") {
-                if (currentSection.content[cursorCoordinates[1] - 1] !== " ") {
-                    if (cursorCoordinates[1] == 0) {
-                        sections = [
-                            ...sections.slice(0, cursorCoordinates[0]),
-                            {
-                                startIndex: currentSection.startIndex,
-                                endIndex: currentSection.startIndex + 1,
-                                type: "superscript",
-                                content: "+"
-                            },
-                            {
-                                startIndex: currentSection.startIndex + 2,
-                                endIndex: currentSection.endIndex + 2,
-                                type: "normal",
-                                content: currentSection.content
-                            },
-                            ...sections.slice(cursorCoordinates[0] + 1).map(s => {
-                                return {
-                                    startIndex: s.startIndex + 2,
-                                    endIndex: s.endIndex + 2,
-                                    type: s.type,
-                                    content: s.content
-                                }
-                            })
-                        ]
-                    } else {
-                        sections = [
-                            ...sections.slice(0, cursorCoordinates[0]),
-                            {
-                                startIndex: currentSection.startIndex,
-                                endIndex: currentSection.startIndex + cursorCoordinates[1],
-                                type: "normal",
-                                content: currentSection.content.slice(0, cursorCoordinates[1])
-                            },
-                            {
-                                startIndex: currentSection.startIndex + cursorCoordinates[1] + 1,
-                                endIndex: currentSection.startIndex + cursorCoordinates[1] + 2,
-                                type: "superscript",
-                                content: "+"
-                            },
-                            {
+            if ((e.data === "+" || e.data === "-") && currentSection.content[cursorCoordinates[1] - 1] !== " " && currentSection.content[cursorCoordinates[1] - 1] !== undefined) {
+                if (cursorCoordinates[1] == 0) {
+                    sections = [
+                        ...sections.slice(0, cursorCoordinates[0]),
+                        {
+                            startIndex: currentSection.startIndex,
+                            endIndex: currentSection.startIndex + 1,
+                            type: "superscript",
+                            content: e.data
+                        },
+                        {
+                            startIndex: currentSection.startIndex + 2,
+                            endIndex: currentSection.endIndex + 2,
+                            type: "normal",
+                            content: currentSection.content
+                        },
+                        ...sections.slice(cursorCoordinates[0] + 1).map(s => {
+                            return {
+                                startIndex: s.startIndex + 2,
+                                endIndex: s.endIndex + 2,
+                                type: s.type,
+                                content: s.content
+                            }
+                        })
+                    ]
+                } else {
+                    const endNumberMatch = currentSection.content.slice(0, cursorCoordinates[1]).match(endNumberRegex)
+                    sections = [
+                        ...sections.slice(0, cursorCoordinates[0]),
+                        {
+                            startIndex: currentSection.startIndex,
+                            endIndex: currentSection.startIndex + cursorCoordinates[1] - endNumberMatch[1].length,
+                            type: "normal",
+                            content: currentSection.content.slice(0, cursorCoordinates[1] - endNumberMatch[1].length)
+                        },
+                        {
+                            startIndex: currentSection.startIndex + cursorCoordinates[1] + 1 - endNumberMatch[1].length,
+                            endIndex: currentSection.startIndex + cursorCoordinates[1] + 2,
+                            type: "superscript",
+                            content: endNumberMatch[1] + e.data
+                        },
+                        ...(currentSection.content.slice(cursorCoordinates[1]) ?
+                            [{
                                 startIndex: currentSection.startIndex + cursorCoordinates[1] + 3,
                                 endIndex: currentSection.endIndex + 3,
                                 type: "normal",
                                 content: currentSection.content.slice(cursorCoordinates[1])
-                            },
-                            ...sections.slice(cursorCoordinates[0] + 1).map(s => {
-                                return {
-                                    startIndex: s.startIndex + 2,
-                                    endIndex: s.endIndex + 2,
-                                    type: s.type,
-                                    content: s.content
-                                }
-                            })
-                        ]
-                    }
-                    contentLength++
+                            } as FormulaSection]
+                            : []
+                        ),
+                        ...sections.slice(cursorCoordinates[0] + 1).map(s => {
+                            return {
+                                startIndex: s.startIndex + 3,
+                                endIndex: s.endIndex + 3,
+                                type: s.type,
+                                content: s.content
+                            }
+                        })
+                    ]
                 }
+                contentLength++
+            } else if (!"1234567890".includes(e.data)) {
+                const endNumberMatch = currentSection.content.match(endNumberRegex)
+                sections = [
+                    ...sections.slice(0, cursorCoordinates[0]),
+                    ...(cursorCoordinates[1] ?
+                        [{
+                            startIndex: currentSection.startIndex,
+                            endIndex: currentSection.startIndex + cursorCoordinates[1] - endNumberMatch[1].length,
+                            type: "normal",
+                            content: currentSection.content.slice(0, cursorCoordinates[1] - endNumberMatch[1].length)
+                        } as FormulaSection]
+                        : []
+                    ),
+                    ...(endNumberMatch[1].length ?
+                        [{
+                            startIndex: currentSection.startIndex + cursorCoordinates[1] + (cursorCoordinates[1] ? 1 : 0),
+                            endIndex: currentSection.startIndex + cursorCoordinates[1] + endNumberMatch[1].length + (cursorCoordinates[1] ? 1 : 0) + 1,
+                            type: "subscript",
+                            content: endNumberMatch[1]
+                        } as FormulaSection]
+                        : []
+                    ),
+                    {
+                        startIndex: currentSection.startIndex + cursorCoordinates[1] + (endNumberMatch[1].length ? endNumberMatch[1].length + 3 : 1),
+                        endIndex: currentSection.endIndex + (endNumberMatch[1].length ? endNumberMatch[1].length + 4 : 2),
+                        type: "normal",
+                        content: currentSection.content.slice(cursorCoordinates[1]) + e.data
+                    },
+                    ...sections.slice(cursorCoordinates[0] + 1).map(s => {
+                        return {
+                            startIndex: s.startIndex + (endNumberMatch[1].length ? endNumberMatch[1].length + 3 : 1),
+                            endIndex: s.endIndex + (endNumberMatch[1].length ? endNumberMatch[1].length + 3 : 1),
+                            type: s.type,
+                            content: s.content
+                        }
+                    })
+                ]
             } else {
-                currentSection.content = currentSection.content + e.data
+                currentSection.content = currentSection.content.slice(0, cursorCoordinates[1]) + e.data + currentSection.content.slice(cursorCoordinates[1])
                 currentSection.endIndex++
-                cursorPosition++
                 contentLength++
             }
         } else if (currentSection.type === "superscript") {
             if ("1234567890+-".includes(e.data)) {
-
+                sections = [
+                    ...sections.slice(0, cursorCoordinates[0]),
+                    {
+                        startIndex: currentSection.startIndex,
+                        endIndex: currentSection.endIndex + 1,
+                        type: "superscript",
+                        content: currentSection.content.slice(0, cursorCoordinates[1]) + e.data + currentSection.content.slice(cursorCoordinates[1])
+                    },
+                    ...sections.slice(cursorCoordinates[0] + 1).map(s => {
+                        return {
+                            startIndex: s.startIndex + 2,
+                            endIndex: s.endIndex + 2,
+                            type: s.type,
+                            content: s.content
+                        }
+                    })
+                ]
+            } else {
+                sections = [
+                    ...sections.slice(0, cursorCoordinates[0]),
+                    {
+                        startIndex: currentSection.startIndex,
+                        endIndex: currentSection.startIndex + cursorCoordinates[1],
+                        type: "superscript",
+                        content: currentSection.content.slice(0, cursorCoordinates[1])
+                    },
+                    {
+                        startIndex: currentSection.startIndex + cursorCoordinates[1] + 1,
+                        endIndex: currentSection.startIndex + cursorCoordinates[1] + 2,
+                        type: "normal",
+                        content: e.data
+                    },
+                    ...(currentSection.content.slice(cursorCoordinates[1]) ?
+                        [{
+                            startIndex: currentSection.startIndex + cursorCoordinates[1] + 3,
+                            endIndex: currentSection.endIndex + 3,
+                            type: "superscript",
+                            content: currentSection.content.slice(cursorCoordinates[1])
+                        } as FormulaSection]
+                        : []
+                    ),
+                    ...sections.slice(cursorCoordinates[0] + 1).map(s => {
+                        return {
+                            startIndex: s.startIndex + 3,
+                            endIndex: s.endIndex + 3,
+                            type: s.type,
+                            content: s.content
+                        }
+                    })
+                ]
+            }
+        } else if (currentSection.type === "subscript") {
+            if ("1234567890".includes(e.data)) {
+                sections = [
+                    ...sections.slice(0, cursorCoordinates[0]),
+                    {
+                        startIndex: currentSection.startIndex,
+                        endIndex: currentSection.endIndex + 1,
+                        type: "subscript",
+                        content: currentSection.content.slice(0, cursorCoordinates[1]) + e.data + currentSection.content.slice(cursorCoordinates[1])
+                    },
+                    ...sections.slice(cursorCoordinates[0] + 1).map(s => {
+                        return {
+                            startIndex: s.startIndex + 2,
+                            endIndex: s.endIndex + 2,
+                            type: s.type,
+                            content: s.content
+                        }
+                    })
+                ]
+            } else {
+                sections = [
+                    ...sections.slice(0, cursorCoordinates[0]),
+                    {
+                        startIndex: currentSection.startIndex,
+                        endIndex: currentSection.startIndex + cursorCoordinates[1],
+                        type: "subscript",
+                        content: currentSection.content.slice(0, cursorCoordinates[1])
+                    },
+                    {
+                        startIndex: currentSection.startIndex + cursorCoordinates[1] + 1,
+                        endIndex: currentSection.startIndex + cursorCoordinates[1] + 2,
+                        type: "normal",
+                        content: e.data
+                    },
+                    ...(currentSection.content.slice(cursorCoordinates[1]) ?
+                        [{
+                            startIndex: currentSection.startIndex + cursorCoordinates[1] + 3,
+                            endIndex: currentSection.endIndex + 3,
+                            type: "subscript",
+                            content: currentSection.content.slice(cursorCoordinates[1])
+                        } as FormulaSection]
+                        : []
+                    ),
+                    ...sections.slice(cursorCoordinates[0] + 1).map(s => {
+                        return {
+                            startIndex: s.startIndex + 3,
+                            endIndex: s.endIndex + 3,
+                            type: s.type,
+                            content: s.content
+                        }
+                    })
+                ]
             }
         }
 
